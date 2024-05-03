d280593eb09c93de5edc654ecb4bae9f2696a512
Ben Clark <benhamiltonclark@gmail.com>
2024-05-03 13:52:39 -0400

    Adding final touches to my website... final touches for now at least ;)

--- portfolio.js
+++ portfolio.js
@@ -20,4 +20,44 @@ class CssPropControl {
     bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))
   })
 
-  console.log("Hey there!")
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+  console.log("Hey there! Wanna hear a joke?")
+
+
+
+
+
+
+
+
